b786517e44827fa85e8fec5cf53914252a21a2d5
Shubham Pawar <shubhampawar5929@gmail.com>
2026-08-15 20:38:58 +0530

    Got basic understanding of React Elements and React Functional Components

--- App.js
+++ App.js
@@ -1,31 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>React Element</span>;
 
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent"
-  },
- [
-   React.createElement(
-    "div",
-    {
-      id: "child"
-    },
-    [React.createElement("h1", {}, "This is Namaste React 🚀"), React.createElement("h2", {}, "I am h2 tag")]
-  ),
-   React.createElement(
-    "div",
-    {
-      id: "child2"
-    },
-    [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-  )
- ]
+const title = () => (
+  <h1 id="heading" className="head" tabIndex={1}>
+    {elem}
+    Namste React using JSX 🚀
+  </h1>
 );
 
+const number = 10000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title()}
+    <h1>{number}</h1>
+    <h1 className="heading">Namste React Functional Componenet 🚀</h1>
+    <h2>{2 + 3}</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
+root.render(<HeadingComponent />);
