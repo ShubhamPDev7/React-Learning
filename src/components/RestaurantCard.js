87ba5a2dc3da7bd247094bad07fbef3187b2437d
Shubham Pawar <shubhampawar5929@gmail.com>
2026-08-27 14:10:14 +0530

    learn: add React Router routing and error handling

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -15,7 +15,7 @@ const RestaurantCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
-      <h4>{sla.deliveryTime} mins</h4>
+      <h4>{sla?.slaString}</h4>
       <h4>{costForTwo}</h4>
     </div>
   );
